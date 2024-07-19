10478fc312d1be9397d019e439c6fc90c5258941
Michael <jmafoster1@gmail.com>
2024-07-19 10:19:03 +0100

    fancier mutation operator

--- deap_GP_first.py
+++ deap_GP_first.py
@@ -35,6 +35,9 @@ from numpy import negative, exp, power, log, sin, cos, tan, sinh, cosh, tanh
 
 from operator import attrgetter, add, sub, mul, truediv
 
+random.seed(1)
+
+
 warnings.filterwarnings("error")
 
 pset = gp.PrimitiveSet("MAIN", 1)
@@ -43,15 +46,15 @@ pset.addPrimitive(sub, 2)
 pset.addPrimitive(mul, 2)
 pset.addPrimitive(truediv, 2)
 pset.addPrimitive(negative, 1)
-pset.addPrimitive(cos, 1)
-pset.addPrimitive(sin, 1)
 pset.addEphemeralConstant("rand101", partial(random.randint, -1, 1))
 pset.renameArguments(ARG0="x")
 
-pset.addPrimitive(exp, 1)
-pset.addPrimitive(log, 1)
-pset.addPrimitive(power, 2)
-pset.addPrimitive(tan, 1)
+# pset.addPrimitive(sin, 1)
+# pset.addPrimitive(cos, 1)
+# pset.addPrimitive(tan, 1)
+# pset.addPrimitive(exp, 1)
+# pset.addPrimitive(log, 1)
+# pset.addPrimitive(power, 2)
 # pset.addPrimitive(sinh, 1)
 # pset.addPrimitive(cosh, 1)
 # pset.addPrimitive(tanh, 1)
@@ -67,12 +70,15 @@ toolbox.register("compile", gp.compile, pset=pset)
 
 
 def evalSymbReg(individual, points_x, points_y):
+    print("INDIVIDUAL", individual)
     points_x["y"] = points_y
     try:
         # Create model, fit (run) it, give estimates from it]
         model = smf.ols(f"y ~ {individual}", points_x)
         res = model.fit()
         y_estimates = res.predict(points_x)
+
+        print(pd.concat([points_x, y_estimates], axis=1))
 
         # Calc errors using an improved normalised mean squared
         sqerrors = (points_y - y_estimates) ** 2
@@ -93,22 +99,40 @@ def evalSymbReg(individual, points_x, points_y):
 
 
 points_x = pd.DataFrame({"x": [float(x) for x in range(2, 100)]})
-points_y = pd.Series([(log(x**5) + (x**3 + x**2 + x**0.5) / log(x**3.5) + exp(x)) for x in range(2, 100)])
+points_y = pd.Series([x + 5 for x in range(2, 100)])
+
+solution = gp.PrimitiveTree.from_string("sub(1, x)", pset)
+print(solution, evalSymbReg(solution, points_x, points_y))
+assert False
+
+
+def mutation(individual, expr, pset):
+    choice = random.randint(0, 3)
+    if choice == 0:
+        return gp.mutUniform(individual, expr, pset)
+    elif choice == 1:
+        return gp.mutNodeReplacement(individual, pset)
+    elif choice == 2:
+        return gp.mutInsert(individual, pset)
+    elif choice == 3:
+        return gp.mutShrink(individual)
+    else:
+        return individual
+
 
 toolbox.register("evaluate", evalSymbReg, points_x=points_x, points_y=points_y)
 toolbox.register("select", tools.selTournament, tournsize=3)
 toolbox.register("mate", gp.cxOnePoint)
 toolbox.register("expr_mut", gp.genFull, min_=0, max_=2)
-toolbox.register("mutate", gp.mutUniform, expr=toolbox.expr_mut, pset=pset)
+toolbox.register("mutate", mutation, expr=toolbox.expr_mut, pset=pset)
 
 toolbox.decorate("mate", gp.staticLimit(key=lambda x: x.height + 1, max_value=17))
 toolbox.decorate("mutate", gp.staticLimit(key=lambda x: x.height + 1, max_value=17))
 
 
 def main():
-    random.seed(1)
 
-    pop = toolbox.population(n=300)
+    pop = toolbox.population(n=10)
     hof = tools.HallOfFame(
         1
     )  # to maintain some individuals if using ea/ga that deletes old population on each generation
@@ -121,7 +145,18 @@ def main():
     mstats.register("min", np.min)
     mstats.register("max", np.max)
 
-    pop, log = algorithms.eaMuPlusLambda(pop, toolbox, 10, 50, 0.4, 0.3, 20, stats=mstats, halloffame=hof, verbose=True)
+    pop, log = algorithms.eaMuPlusLambda(
+        pop,
+        toolbox,
+        mu=10,
+        lambda_=5,
+        cxpb=0.5,
+        mutpb=0.5,
+        ngen=20,
+        stats=stats_fit,
+        halloffame=hof,
+        verbose=True,
+    )
 
     gen = log.chapters["fitness"].select("gen")
     min = log.chapters["fitness"].select("min")
@@ -148,4 +183,7 @@ def main():
 
 if __name__ == "__main__":
     pop, log, hof = main()
-    print(pd.DataFrame({"x": [str(x) for x in pop], "fitness": [x.fitness for x in pop]}))
+    print(len(pop))
+    print(
+        pd.DataFrame({"x": [str(x) for x in pop], "fitness": [x.fitness for x in pop]})
+    )
