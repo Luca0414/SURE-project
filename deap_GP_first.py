b9522146c60a6eff769af35e495068aa6558059a
Luca Devlin <luca.devlin@btinternet.com>
2024-07-19 17:46:13 +0200

    failing to mix mine and michaels changes manually. I shouldve just used git from start D:

--- deap_GP_first.py
+++ deap_GP_first.py
@@ -1,53 +1,96 @@
-# RQ1: To what extent does the use of GP for linear regression create a more accurate computational model?
+# RQ1: To what extent does the use of GP for linear regression create a more accurate computational model than pure GP or linear regression?
 # RQ2: How does changing the operators used affect the accuracy of our computational model?
 # RQ3: How does changing the size of the data set used affect the accuracy of our computational model?
 # RQ4: How does the use of a noisy data set affect the accuracy of our computational model?
 
-# Accuracy: How many generations for fitness = 0 or if gen > 40 use nmse - I need to figure out how to give a concrete score that combines these ideas
+# Accuracy: How many generations for fitness = 0 or if gen > 40 use min nmse - Will have to use two figures to show these two outcomes
 
 # Experiment: 
-# 1. Create a list of 10 equations of different complexity and using different operators. 
-# 2. Run 25 different seeds for each equation using my mix of GP + linear regression. 
-# 2.1 RQ2: 4 using just add, sub, mul, div. 4 adding neg, cos, sin + random. 5 using all. 
-# 2.2 RQ3: 7 using 10/25/50/100/250/600/1000) data points in range x (2-1002 randomly). 
-# 2.3 RQ4: 5 using noisty data.
-# 3. Run those 25 seeds using just GP.
-# 4. Run those 25 seeds using just linear regression
+# 1. Create a list of 10(00 in real) equations of different complexity and using different operators. # Use benchmarks given
+# 2. Run 30 different seeds for each version.
+# 2.1 RQ2: Using just add, sub, mul, div, neg + reciprocal. Adding sin, cos, tan, log, root + square. Adding exponential, cube, 4th power, sinh, cosh + tanh. 
+# 2.2 RQ3: Using 10/100/1000 data points randomly for each variable. 
+# 2.3 RQ4: Using noisty data.
+# 3. Run those 150 seeds using just GP.
+# 4. Run those 150 seeds using just linear regression.
 
 # Ideas:
 # Syntatic closenose (RQ5?) - have to research
 # If a regression coefficient close to 0 = remove - post processing
+# Re-add 25/50/250/500 data sizes?
+# Justify use of operators + data numbers
+# Look into more than one variable input (Michael looking into it)
+# save to json instead of default when using bash
 
-import operator
-import math
 import random
+import warnings
+import patsy
+import argparse
+import statsmodels
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import statsmodels.api as sm
+import statsmodels.formula.api as smf
 
-from functools import partial
-from deap import algorithms, base, creator, tools, gp
+from deap import base, creator, tools, gp
+from numpy import negative, exp, log, sin, cos, tan, sinh, cosh, tanh
+from operator import add, sub, mul, truediv
+
+random.seed(1) # change to use argument
+
+
+warnings.filterwarnings("error")
+
+# parser = argparse.ArgumentParser(description="Run experiments")
+# parser.add_argument('--seed', type=int, required=True, help='Random seed')
+# parser.add_argument('--equation', type=str, required=True, help='Equation to use')
+# parser.add_argument('--operators', type=str, required=True, help='Operators to include')
+# parser.add_argument('--data_points', type=int, required=True, help='Number of data points')
+# parser.add_argument('--noise', type=float, required=True, help='Noise level')
+# parser.add_argument('--method', type=str, required=True, choices=['GP', 'LR', 'GPLR'], help='Method to use (GP, LR)')
+
+# args = parser.parse_args()
+
+# Define more operators
+def root(x):
+    return x ** 0.5
+
+def square(x):
+    return x ** 2
+
+def cube(x):
+    return x ** 3
+
+def fourth_power(x):
+    return x ** 4
+
+def reciprocal(x):
+    return 1 / x
 
 pset = gp.PrimitiveSet("MAIN", 1)
-pset.addPrimitive(operator.add, 2)
-pset.addPrimitive(operator.sub, 2)
-pset.addPrimitive(operator.mul, 2)
-pset.addPrimitive(operator.truediv, 2)
-pset.addPrimitive(operator.neg, 1)
-pset.addPrimitive(math.cos, 1)
-pset.addPrimitive(math.sin, 1)
-pset.addEphemeralConstant("rand101", partial(random.randint, -1, 1))
 pset.renameArguments(ARG0='x')
 
-pset.addPrimitive(math.exp, 1)
-pset.addPrimitive(math.log, 1)
-pset.addPrimitive(math.pow, 2)
-pset.addPrimitive(math.tan, 1)
-pset.addPrimitive(math.sinh, 1)
-pset.addPrimitive(math.cosh, 1)
-pset.addPrimitive(math.tanh, 1)
+pset.addPrimitive(add, 2)
+pset.addPrimitive(sub, 2)
+pset.addPrimitive(mul, 2)
+pset.addPrimitive(truediv, 2)
+pset.addPrimitive(negative, 1)
+pset.addPrimitive(reciprocal, 1)
+
+pset.addPrimitive(sin, 1)
+pset.addPrimitive(cos, 1)
+pset.addPrimitive(tan, 1)
+pset.addPrimitive(log, 1)
+pset.addPrimitive(root, 1)
+pset.addPrimitive(square, 1)
+
+pset.addPrimitive(exp, 1)
+pset.addPrimitive(cube, 1)
+pset.addPrimitive(fourth_power, 1)
+pset.addPrimitive(sinh, 1)
+pset.addPrimitive(cosh, 1)
+pset.addPrimitive(tanh, 1)
 
 creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
 creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMin)
@@ -70,55 +113,163 @@ def split(individual):
         return terms
     return [individual]
 
-def evalSymbReg(individual, points_x, points_y):
+def repair(individual, points_x, points_y):
+    eq = f"y ~ {' + '.join(str(x) for x in split(individual))}"
+    df = pd.concat((points_x, points_y.rename("y")), axis=1)
     try:
-        terms = split(individual)
-
-        # Create Exog variables as a pandas DataFrame
-        data = []
-        for x in points_x:
-            values = []
-            for term in terms:
-                func  = toolbox.compile(expr=term)
-                values.append(func(x))
-            values.append(1) #add intercepts
-            data.append(values)
-        
-        df = pd.DataFrame(data)        
-
-        # Create model, fit (run) it, give estimates from it
-        model = sm.OLS(points_y, df)
+        # Create model, fit (run) it, give estimates from it]
+        model = smf.ols(eq, df)
         res = model.fit()
         y_estimates = res.predict(df)
 
-        # Calc errors using an improved normalised mean squared
-        sqerrors = (y_estimates - points_y)**2
-        mean_squared = math.fsum(sqerrors) / len(points_x)
-        nmse = mean_squared / (math.fsum(points_y)/ len(points_y))
+        eqn = f"{res.params['Intercept']}"
+        for term, coefficient in res.params.items():
+            if term != "Intercept":
+                eqn = f"add({eqn}, mul({coefficient}, {term}))"
+        repaired = type(individual)(gp.PrimitiveTree.from_string(eqn, pset))
+        return repaired
+    except (
+        OverflowError,
+        ValueError,
+        ZeroDivisionError,
+        statsmodels.tools.sm_exceptions.MissingDataError,
+        patsy.PatsyError,
+    ) as e:
+        return individual
 
-        return nmse,
+def evalSymbReg(individual, points_x, points_y):
+    try:
+        # Create model, fit (run) it, give estimates from it]
+        func = gp.compile(individual, pset)
+        y_estimates = pd.Series([func(**x) for _, x in points_x.iterrows()])
+
+        # Calc errors using an improved normalised mean squared
+        sqerrors = (points_y - y_estimates) ** 2
+        mean_squared = sqerrors.sum() / len(points_x)
+        nmse = mean_squared / (points_y.sum() / len(points_y))
+
+        return (nmse,)
 
         # Fitness value of infinite if error - not return 1
-    except (OverflowError, ValueError, ZeroDivisionError):
-        return 10**100,
+    except (
+        OverflowError,
+        ValueError,
+        ZeroDivisionError,
+        statsmodels.tools.sm_exceptions.MissingDataError,
+        patsy.PatsyError,
+        RuntimeWarning,
+    ) as e:
+        print(e)
+        return (float("inf"),)
 
-points_x = [x for x in range(2, 100)]
-points_y = [(math.log(x**5) + (x**3 + x**2 + x**0.5)/math.log(x**3.5) + math.exp(x)) for x in range(2, 100)]
+def make_offspring(population, toolbox, lambda_):
+    offspring = []
+    for i in range(lambda_):
+        parent1, parent2 = tools.selTournament(population, 2, 2)
+        child, _ = toolbox.mate(toolbox.clone(parent1), toolbox.clone(parent2))
+        del child.fitness.values
+        (child,) = toolbox.mutate(child)
+        offspring.append(child)
+    return offspring
+
+
+def eaMuPlusLambda(
+    population,
+    toolbox,
+    mu,
+    lambda_,
+    ngen,
+    stats=None,
+    halloffame=None,
+    verbose=__debug__,
+):
+    population = [toolbox.repair(ind) for ind in population]
+
+    logbook = tools.Logbook()
+    logbook.header = ["gen", "nevals"] + (stats.fields if stats else [])
+
+    # Evaluate the individuals with an invalid fitness
+    for ind in population:
+        ind.fitness.values = toolbox.evaluate(ind)
+
+    if halloffame is not None:
+        halloffame.update(population)
+
+    record = stats.compile(population) if stats is not None else {}
+    logbook.record(gen=0, nevals=len(population), **record)
+    if verbose:
+        print(logbook.stream)
+
+    # Begin the generational process
+    for gen in range(1, ngen + 1):
+
+        # Vary the population
+        offspring = make_offspring(population, toolbox, lambda_)
+        offspring = [toolbox.repair(ind) for ind in offspring]
+
+        # Evaluate the individuals with an invalid fitness
+        for ind in offspring:
+            ind.fitness.values = toolbox.evaluate(ind)
+
+        # Update the hall of fame with the generated individuals
+        if halloffame is not None:
+            halloffame.update(offspring)
+
+        # Select the next generation population
+        population[:] = toolbox.select(population + offspring, mu)
+
+        # Update the statistics with the new population
+        record = stats.compile(population) if stats is not None else {}
+        logbook.record(gen=gen, nevals=len(offspring), **record)
+        if verbose:
+            print(logbook.stream)
+
+    return population, logbook
+
+
+points_x = pd.DataFrame({"x": [float(x) for x in range(2, 100)]})
+points_y = pd.Series([x**2 + x + 5 for x in range(2, 100)])
+
+solution = gp.PrimitiveTree.from_string("add(square(x), x)", pset)
+print(repair(solution, points_x, points_y))
+print(solution, evalSymbReg(solution, points_x, points_y))
+
+
+def mutation(individual, expr, pset):
+    choice = random.randint(0, 3)
+    if choice == 0:
+        mutated = gp.mutUniform(toolbox.clone(individual), expr, pset)
+    elif choice == 1:
+        mutated = gp.mutNodeReplacement(toolbox.clone(individual), pset)
+    elif choice == 2:
+        mutated = gp.mutInsert(toolbox.clone(individual), pset)
+    elif choice == 3:
+        mutated = gp.mutShrink(toolbox.clone(individual))
+    else:
+        raise ValueError("Invalid mutation choice")
+    return mutated
+
 
 toolbox.register("evaluate", evalSymbReg, points_x=points_x, points_y=points_y)
-toolbox.register("select", tools.selTournament, tournsize=3)
+toolbox.register("repair", repair, points_x=points_x, points_y=points_y)
+toolbox.register("select", tools.selBest)
 toolbox.register("mate", gp.cxOnePoint)
 toolbox.register("expr_mut", gp.genFull, min_=0, max_=2)
-toolbox.register("mutate", gp.mutUniform, expr=toolbox.expr_mut, pset=pset)
+toolbox.register("mutate", mutation, expr=toolbox.expr_mut, pset=pset)
 
-toolbox.decorate("mate", gp.staticLimit(key=operator.attrgetter("height"), max_value=17))
-toolbox.decorate("mutate", gp.staticLimit(key=operator.attrgetter("height"), max_value=17))
+toolbox.decorate("mate", gp.staticLimit(key=lambda x: x.height + 1, max_value=17))
+toolbox.decorate("mutate", gp.staticLimit(key=lambda x: x.height + 1, max_value=17))
+
 
 def main():
-    random.seed(1)
 
-    pop = toolbox.population(n=300)
-    hof = tools.HallOfFame(1) # to maintain some individuals if using ea/ga that deletes old population on each generation
+    mu = 20
+
+    pop = toolbox.population(n=mu)
+
+    hof = tools.HallOfFame(
+        1
+    )  # to maintain some individuals if using ea/ga that deletes old population on each generation
 
     stats_fit = tools.Statistics(lambda ind: ind.fitness.values)
     stats_size = tools.Statistics(len)
@@ -128,8 +279,16 @@ def main():
     mstats.register("min", np.min)
     mstats.register("max", np.max)
 
-    pop, log = algorithms.eaMuPlusLambda(pop, toolbox, 10, 50, 0.4, 0.3, 40, stats=mstats,
-                                   halloffame=hof, verbose=True)
+    pop, log = eaMuPlusLambda(
+        pop,
+        toolbox,
+        mu=mu,
+        lambda_=5,
+        ngen=100,
+        stats=None,
+        halloffame=hof,
+        verbose=True,
+    )
 
     gen = log.chapters["fitness"].select("gen")
     min = log.chapters["fitness"].select("min")
@@ -148,10 +307,15 @@ def main():
     labs = [l.get_label() for l in lns]
     ax1.legend(lns, labs, loc="center right")
 
-    plt.show()
+    # plt.show()
 
     # print log
     return pop, log, hof
 
+
 if __name__ == "__main__":
-    main()
+    pop, log, hof = main()
+    print(len(pop))
+    print(
+        pd.DataFrame({"x": [str(x) for x in pop], "fitness": [x.fitness for x in pop]})
+    )
---
+++ run_experiment.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+run_gp() {
+    seed=$1
+    eq=$2
+    op=$3
+    dp=$4
+    noise=$5
+    python deap_GP_first.py --seed $seed --equation "$eq" --operators "$op" --data_points $dp --noise $noise --method "GP"
+}
+
+run_lr() {
+    seed=$1
+    eq=$2
+    op=$3
+    dp=$4
+    noise=$5
+    python deap_GP_first.py --seed $seed --equation "$eq" --operators "$op" --data_points $dp --noise $noise --method "LR"
+}
+
+run_gplr() {
+    seed=$1
+    eq=$2
+    op=$3
+    dp=$4
+    noise=$5
+    python deap_GP_first.py --seed $seed --equation "$eq" --operators "$op" --data_points $dp --noise $noise --method "GPLR"
+}
+
+equations=(
+    "x**2 + x + 1"
+    "x**3 - x + 2"
+    "x**2 - 4*x + 4"
+    "2*x**2 + 3*x + 5"
+    "x**4 + x**3 + x**2 + x + 1"
+    "x**2 * math.sin(x)"
+    "math.cos(x) + x**3"
+    "math.exp(x) - x**2"
+    "math.log(x) + x"
+    "x**3 - 3*x**2 + 3*x - 1"
+)
+
+# Number of seeds
+seeds=30
+
+# RQ2 configurations
+operators1="operator.add operator.sub operator.mul operator.truediv"
+operators2="operator.add operator.sub operator.mul operator.truediv operator.neg math.cos math.sin"
+operators3="operator.add operator.sub operator.mul operator.truediv operator.neg math.cos math.sin math.log math.sqrt square cube fourth_power"
+
+# RQ3 configurations
+data_points=(10 100 1000)
+
+# RQ4 configurations
+noise_levels=(0 0.1 0.2 0.3)
+
+
+for eq in "${equations[@]}"; do
+    for seed in $(seq 1 $seeds); do
+        for op in "$operators1" "$operators2" "$operators3"; do
+            for dp in "${data_points[@]}"; do
+                for noise in "${noise_levels[@]}"; do
+                    run_gp $seed "$eq" "$op" $dp $noise
+                    run_lr $seed "$eq" "$op" $dp $noise
+                    run_gplr $seed "$eq" "$op" $dp $noise
+                done
+            done
+        done
+    done
+done
