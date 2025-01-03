be1510a8933368884bf0e117b25729e5e3b8e714
Michael <jmafoster1@gmail.com>
2025-01-03 14:45:20 +0000

    Generate and run

---
+++ expression_generator.py
@@ -0,0 +1,119 @@
+from deap import base, creator, tools, gp
+from operator import add, sub, mul, truediv
+from functools import reduce
+import pandas as pd
+import numpy as np
+import random
+import json
+
+def root(x):
+    return x**0.5
+
+
+def power_2(x):
+    return x**2
+
+
+def power_3(x):
+    return x**3
+
+
+def reciprocal(x):
+    return 1 / x
+
+class ExpressionGenerator():
+    def __init__(self, n_var, random_seed=0):
+        np.random.seed(random_seed)
+        random.seed(random_seed)
+
+        self.n_var = n_var
+
+        # Setup the primitive set
+        self.pset = gp.PrimitiveSetTyped("main", [float] * n_var, float)
+        self.pset.renameArguments(**{f"ARG{i}": f"x{i}" for i in range(n_var)})
+        self.pset.addPrimitive(add, [float, float], float)
+        self.pset.addPrimitive(mul, [float, float], float)
+        self.pset.addPrimitive(np.sin, [float], float)
+        self.pset.addPrimitive(np.cos, [float], float)
+        self.pset.addPrimitive(np.tan, [float], float)
+        self.pset.addPrimitive(np.log, [float], float)
+        self.pset.addPrimitive(reciprocal, [float], float)
+        self.pset.addPrimitive(root, [float], float)
+        self.pset.addPrimitive(power_2, [float], float)
+        self.pset.addPrimitive(power_3, [float], float)
+
+        creator.create("Individual", gp.PrimitiveTree)
+
+        self.toolbox = base.Toolbox()
+        self.toolbox.register("expr", gp.genFull, pset=self.pset, min_=0, max_=4)
+        self.toolbox.register(
+            "individual", tools.initIterate, creator.Individual, self.toolbox.expr
+        )
+        self.toolbox.register("compile", gp.compile, pset=self.pset)
+
+
+
+    def make_term(self, var):
+        term = self.toolbox.individual()
+        _, _, labels = gp.graph(term)
+        if var in labels.values():
+            return term
+        terminals = [
+            k for k, v in labels.items() if isinstance(self.pset.mapping[v], gp.Terminal)
+        ]
+        index = 0 if len(terminals) == 0 else random.choice(terminals)
+        term[index] = self.pset.mapping[var]
+        return term
+
+    def generate_training_data(self, individual, size:int, noise_factor:float=None):
+        individual = creator.Individual(gp.PrimitiveTree.from_string(individual, self.pset))
+        func = self.toolbox.compile(expr=individual)
+        data = pd.DataFrame(
+            np.random.uniform(0, 100, size=(size, self.n_var)),
+            columns=[f"x{n}" for n in range(self.n_var)],
+        )
+        outputs = np.array([func(**row.to_dict()) for _, row in data.iterrows()])
+
+        noise = np.zeros(len(outputs))
+        if noise_factor is not None:
+            noise = np.random.uniform(-1, 1, size=array.size) * noise_factor * array
+
+        return data, outputs + noise
+
+    def generate_expression(self):
+        terms = [str(self.make_term(arg)) for arg in self.pset.arguments]
+        terms = [f"mul({np.random.uniform(-10, 10)}, {term})" for term in terms]
+        individual = reduce(
+            lambda acc, term: f"add({acc}, {term})", terms, random.uniform(-10, 10)
+        )
+        return individual
+
+if __name__ == "__main__":
+    index = 0
+    np.random.seed(1)
+    random.seed(1)
+
+    experimental_runs = []
+    for n_var in range(1, 11):
+        generator = ExpressionGenerator(n_var)
+        # Generate 30 expressions for each number of variables
+        for _ in range(30):
+            is_null = True
+            while is_null:
+                individual = generator.generate_expression()
+                data, outputs = generator.generate_training_data(individual)
+
+                # If there are any null values or if we get a trivially constant output, try again
+                is_null = data.isnull().any().any() or len(set(outputs)) == 1
+            experimental_runs.append(
+                {
+                    "index": index,
+                    "formula": str(individual),
+                    "num_vars": n_var,
+                    "training_data": data[[f"x{n}" for n in range(n_var)]].to_dict(),
+                    "outputs": outputs,
+                }
+            )
+            index += 1
+    with open("equations.json", 'w') as f:
+        json.dump(experimental_runs, f)
--- learn_equations.py
+++ learn_equations.py
@@ -1,30 +1,21 @@
-from causal_testing.estimation.genetic_programming_regression_fitter import GP
 import json
 import argparse
-from make_linear_equations import root, reciprocal
 import pandas as pd
 import numpy as np
 import deap
 from functools import reduce
 import statsmodels.formula.api as smf
 from time import time
+import os
+
+from causal_testing.estimation.genetic_programming_regression_fitter import GP
+from expression_generator import ExpressionGenerator, root, reciprocal
 
 
-def add_noise(array, noise_factor):
-    """
-    Adds noise to each element of a numpy array.
-
-    Args:
-      array: The input numpy array.
-      noise_factor: The percentage of noise to add to each element (between 0 and 1).
-
-    Returns:
-      A new numpy array with noise added to each element.
-    """
-    if noise_factor == 0:
-        return array
-    noise = np.random.uniform(-1, 1, size=array.size) * noise_factor * array
-    return array + noise
+def time_execution(fun):
+    start_time = time()
+    result = fun()
+    return result, time() - start_time
 
 
 parser = argparse.ArgumentParser(
@@ -32,111 +23,112 @@ parser = argparse.ArgumentParser(
     description="Learn the equations from the supplied JSON file.",
 )
 parser.add_argument(
-    "-i",
-    "--index",
+    "-n",
+    "--num_vars",
     type=int,
-    help="The index of the equation to learn, defaults to all.",
+    required=True,
+    help="The number of variables in an expression.",
 )
 parser.add_argument(
-    "-n",
-    "--num_points",
+    "-d",
+    "--data_size",
     type=int,
+    required=True,
     help="The number of data points to use for training data.",
 )
 parser.add_argument(
     "-e", "--epsilon", type=float, default=0, help="The noise factor. Defaults to 0."
 )
 parser.add_argument("-o", "--outfile", help="Where to save the output.")
-parser.add_argument("-s", "--seed", default=0, help="Random seed.")
-parser.add_argument(
-    "equations", help="The JSON file containing the equations and training data."
-)
+parser.add_argument("-s", "--seed", type=int, default=0, help="Random seed.")
 
 args = parser.parse_args()
 
-with open(args.equations) as f:
-    equations = json.load(f)
 
-if args.index is not None:
-    equations = [equations[args.index]]
+generator = ExpressionGenerator(args.num_vars, args.seed)
+# Keep trying to generate expressions until we get one that properly evaluates.
+# We may get null values if we have a divide by zero error or something.
+isnull = True
+while isnull:
+    expression = generator.generate_expression()
+    df, outputs = generator.generate_training_data(expression, args.data_size)
+    df["outputs"] = outputs
+    isnull = df.isnull().any().any()
+    print(expression)
+    print(df)
 
-for equation in equations:
-    equation["epsilon"] = args.epsilon
+# Seed the population with the standard LR formula
+features = [f"x{i}" for i in range(args.num_vars)]
 
-    df = pd.DataFrame(equation["training_data"]).reset_index()
-    df["outputs"] = add_noise(np.array(equation["outputs"]), args.epsilon)
+model, lr_time = time_execution(
+    lambda: smf.ols(f"outputs ~ {'+'.join(features)}", df).fit()
+)
 
-    if args.num_points is None:
-        equation["num_points"] = len(df)
-    else:
-        equation["num_points"] = args.num_points
-        df = df.sample(args.num_points, random_state=args.seed, replace=False)
+terms = [f"mul({model.params[feature]}, {feature})" for feature in features]
+lr_formula = reduce(
+    lambda acc, term: f"add({acc}, {term})", terms, model.params["Intercept"]
+)
 
-    # Seed the population with the standard LR formula
-    features = [f"x{i}" for i in range(equation["num_vars"])]
+gp = GP(
+    df=df,
+    features=features,
+    outcome="outputs",
+    max_order=3,
+    extra_operators=[
+        (root, 1),
+        (reciprocal, 1),
+        (np.sin, 1),
+        (np.cos, 1),
+        (np.tan, 1),
+        (np.log, 1),
+    ],
+    sympy_conversions={
+        "log": lambda x: f"Log({x},-1)",
+        "root": lambda x: f"sqrt({x})",
+    },
+    seed=args.seed,
+)
 
-    lr_start_time = time()
-    model = smf.ols(f"outputs ~ {'+'.join(features)}", df).fit()
-    lr_time = time() - lr_start_time
+gp_lr_result, gp_lr_time = time_execution(
+    lambda: gp.run_gp(ngen=100, seeds=[lr_formula])
+)
+gp_seed_result, gp_seed_time = time_execution(
+    lambda: gp.run_gp(ngen=100, seeds=[lr_formula], repair=False)
+)
+gp_no_seed_result, gp_no_seed_time = time_execution(
+    lambda: gp.run_gp(ngen=100, seeds=[], repair=False)
+)
 
-    terms = [f"mul({model.params[feature]}, {feature})" for feature in features]
-    lr_formula = reduce(
-        lambda acc, term: f"add({acc}, {term})", terms, model.params["Intercept"]
-    )
-
-    gp = GP(
-        df=df,
-        features=features,
-        outcome="outputs",
-        max_order=3,
-        extra_operators=[
-            (root, 1),
-            (reciprocal, 1),
-            (np.sin, 1),
-            (np.cos, 1),
-            (np.tan, 1),
-            (np.log, 1),
-        ],
-        sympy_conversions={
-            "log": lambda x: f"Log({x},-1)",
-            "root": lambda x: f"sqrt({x})",
-        },
-        seed=args.seed,
-    )
-
-    gp_start_time = time()
-    gp_lr_result = gp.run_gp(ngen=100, seeds=[lr_formula])
-    gp_lr_end_time = time()
-    gp_no_repair_seed_result = gp.run_gp(ngen=100, seeds=[lr_formula], repair=False)
-    gp_no_repair_seed_end_time = time()
-    gp_no_repair_no_seed_result = gp.run_gp(ngen=100, seeds=[], repair=False)
-    gp_no_repair_no_seed_end_time = time()
-
-    equation["simplified_formula"] = str(gp.simplify(equation["formula"]))
-
-    equation["lr_raw_formula"] = str(lr_formula)
-    equation["lr_simplified_formula"] = str(gp.simplify(lr_formula))
-    equation["lr_nrmse"] = gp.fitness(lr_formula)[0]
-    equation["lr_time"] = lr_time
-
-    equation["gp_lr_raw_formula"] = str(gp_lr_result)
-    equation["gp_lr_simplified_formula"] = str(gp.simplify(gp_lr_result))
-    equation["gp_lr_nrmse"] = gp_lr_result.fitness.values[0]
-    equation["gp_lr_time"] = gp_lr_end_time - gp_start_time
-
-    equation["gp_no_repair_seed_raw_formula"] = str(gp_no_repair_seed_result)
-    equation["gp_no_repair_seed_simplified_formula"] = str(gp.simplify(gp_no_repair_seed_result))
-    equation["gp_no_repair_seed_nrmse"] = gp_no_repair_seed_result.fitness.values[0]
-    equation["gp_no_repair_seed_time"] = gp_no_repair_seed_end_time - gp_lr_end_time
-
-    equation["gp_no_repair_no_seed_raw_formula"] = str(gp_no_repair_no_seed_result)
-    equation["gp_no_repair_no_seed_simplified_formula"] = str(gp.simplify(gp_no_repair_no_seed_result))
-    equation["gp_no_repair_no_seed_nrmse"] = gp_no_repair_no_seed_result.fitness.values[0]
-    equation["gp_no_repair_no_seed_end_time"] = gp_no_repair_no_seed_end_time - gp_no_repair_seed_end_time
-
-    equation.pop("training_data")
-    equation["outputs"] = df["outputs"].to_list()
+result = {
+    "raw_target_expression": expression,
+    "simplified_target_expression": str(gp.simplify(expression)),
+    "num_vars": args.num_vars,
+    "epsilon": args.epsilon,
+    "data_size": args.data_size,
+    # Linear regression results
+    "lr_raw_formula": str(lr_formula),
+    "lr_simplified_formula": str(gp.simplify(lr_formula)),
+    "lr_nrmse": gp.fitness(lr_formula)[0],
+    "lr_time": lr_time,
+    # Our GP results
+    "gp_lr_raw_formula": str(gp_lr_result),
+    "gp_lr_simplified_formula": str(gp.simplify(gp_lr_result)),
+    "gp_lr_nrmse": gp.fitness(gp_lr_result)[0],
+    "gp_lr_time": gp_lr_time,
+    # Baseline GP (with seed) results
+    "gp_seed_raw_formula": str(gp_seed_result),
+    "gp_seed_simplified_formula": str(gp.simplify(gp_seed_result)),
+    "gp_seed_nrmse": gp.fitness(gp_seed_result)[0],
+    "gp_seed_time": gp_seed_time,
+    # Baseline GP (without seed) results
+    "gp_no_seed_raw_formula": str(gp_no_seed_result),
+    "gp_no_seed_simplified_formula": str(gp.simplify(gp_no_seed_result)),
+    "gp_no_seed_nrmse": gp.fitness(gp_no_seed_result)[0],
+    "gp_no_seed_time": gp_no_seed_time,
+}
 
 if args.outfile:
-    with open(args.outfile, 'w') as f:
-        json.dump(equations, f)
+    if not os.path.exists(os.path.dirname(args.outfile)):
+        os.mkdir(os.path.dirname(args.outfile))
+    with open(args.outfile, "w") as f:
+        json.dump(result, f)
