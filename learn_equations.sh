ab6686765f3aed76636ddedeccb94703ac157df0
Michael Foster <m.foster@sheffield.ac.uk>
2025-01-07 09:15:11 +0000

    HPC run script

--- learn_equations.sh
+++ learn_equations.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --mem=24000
+#SBATCH --time=8:00:00
+
+echo sbatch learn_equations.sh $@
+
 configurations=$1
 line=$2
+
+module load Anaconda3/5.3.0
+source activate gplr
+
 python learn_equations.py $(sed -n "${line} p" $configurations)
+
+echo "__COMPLETED__"
