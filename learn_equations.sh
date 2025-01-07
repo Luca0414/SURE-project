93815bec3c5c1d6d1c3f44fc5598176305e124f7
Michael Foster <m.foster@sheffield.ac.uk>
2025-01-07 09:17:37 +0000

    module load

--- learn_equations.sh
+++ learn_equations.sh
@@ -8,7 +8,7 @@ echo sbatch learn_equations.sh $@
 configurations=$1
 line=$2
 
-module load Anaconda3/5.3.0
+module load Anaconda3/2019.07
 source activate gplr
 
 python learn_equations.py $(sed -n "${line} p" $configurations)
