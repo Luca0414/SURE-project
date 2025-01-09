d225b8f3fd301fc6e258518061f130ab317c85ee
Michael Foster <m.foster@sheffield.ac.uk>
2025-01-09 13:45:08 +0000

    Plotting

--- plotting/grouped_boxplot.py
+++ plotting/grouped_boxplot.py
@@ -3,9 +3,11 @@ This module provides common code to define a grouped boxplot.
 """
 
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
-from matplotlib.patches import Polygon
+from scipy.stats import friedmanchisquare
+from scikit_posthocs import posthoc_nemenyi_friedman
 
 
 def color(colour, flierprops=None):
@@ -42,6 +44,10 @@ def plot_grouped_boxplot(
         raise ValueError("If providing labels, please ensure that you provide as many as you have plots")
     if isinstance(colours, list) and len(colours) != plots:
         raise ValueError("If providing colours, please ensure that you provide as many as you have plots")
+    if isinstance(showfliers, bool):
+        showfliers = [showfliers] * len(groups)
+    if isinstance(showfliers, list) and len(showfliers) != plots:
+        raise ValueError("If providing showfliers, please ensure that you provide as many as you have plots")
     for i, boxes in enumerate(groups):
         marker = markers[i] if isinstance(markers, list) else markers if markers is not None else "o"
 
@@ -49,7 +55,7 @@ def plot_grouped_boxplot(
             boxes,
             positions=np.array(range(positions)) * (plots + 1) + i,
             widths=width,
-            showfliers=showfliers,
+            showfliers=showfliers[i],
             label=labels[i] if labels is not None else None,
             **color(
                 colours[i] if colours is not None else None,
@@ -75,61 +81,24 @@ def plot_grouped_boxplot(
         plt.clf()
 
 
-def bag_plot(
-    x,
-    y,
-    label=None,
-    savepath=None,
-    colour=None,
-    marker=None,
-    title=None,
-    xlabel=None,
-    ylabel=None,
-):
-    # Calculate quartiles for both X and Y
-    q1_x = np.percentile(x, 25)
-    q3_x = np.percentile(x, 75)
-    q1_y = np.percentile(y, 25)
-    q3_y = np.percentile(y, 75)
-
-    iqr_x = q3_x - q1_x
-    iqr_y = q3_y - q1_y
-
-    q1_minus_x = q1_x - (1.5 * iqr_x)
-    q3_plus_x = q3_x + (1.5 * iqr_x)
-    q1_minus_y = q1_y - (1.5 * iqr_y)
-    q3_plus_y = q3_y + (1.5 * iqr_y)
-
-    # Calculate median
-    median_x = np.median(x)
-    median_y = np.median(y)
-
-    # Create a polygon for the Q1-Q3 region
-    polygon = plt.Polygon([(q1_x, q1_y), (q1_x, q3_y), (q3_x, q3_y), (q3_x, q1_y)], alpha=0.2)
-    polygon.set_color(colour)
-    plt.gca().add_patch(polygon)
-    polygon = plt.Polygon(
-        [(q1_minus_x, q1_minus_y), (q1_minus_x, q3_plus_y), (q3_plus_x, q3_plus_y), (q3_plus_x, q1_minus_y)], alpha=0.2
-    )
-    polygon.set_color(colour)
-    plt.gca().add_patch(polygon)
-
-    # Plot the median point
-    plt.scatter(x, y, color=colour, marker=marker, label=label)
-    plt.scatter(median_x, median_y, color="black", marker=marker, s=50)
-
-    # Set plot limits and labels
-    plt.xlim(min(x) - 0.1, max(x) + 0.1)
-    plt.ylim(min(y) - 0.1, max(y) + 0.1)
-
-    if title is not None:
-        plt.title(title)
-    if xlabel is not None:
-        plt.xlabel(xlabel)
-    if ylabel is not None:
-        plt.ylabel(ylabel)
-
-    if savepath is not None:
-        plt.legend()
-        plt.savefig(savepath)
-        plt.clf()
+def compute_stats(df, groupby, p_alpha, save_path, outcome="nrmse"):
+    stats = [
+        (gp_inx, friedmanchisquare(group[f"lr_{outcome}"], group[f"gp_seed_{outcome}"], group[f"gp_lr_{outcome}"]))
+        for gp_inx, group in df.groupby(groupby)
+    ]
+    stats_data = []
+    for gp_inx, stat in stats:
+        datum = {groupby: gp_inx, "friedman_pvalue": stat.pvalue}
+        if stat.pvalue < p_alpha:
+            nemenyi = posthoc_nemenyi_friedman(
+                df.loc[df[groupby] == gp_inx, [f"lr_{outcome}", f"gp_seed_{outcome}", f"gp_lr_{outcome}"]]
+            )
+            datum["gp_lr/lr"] = nemenyi[f"gp_lr_{outcome}"][f"lr_{outcome}"]
+            datum["gp_lr/gp"] = nemenyi[f"gp_lr_{outcome}"][f"gp_seed_{outcome}"]
+        else:
+            datum["gp_lr/lr"] = None
+            datum["gp_lr/gp"] = None
+        stats_data.append(datum)
+    stats_data = pd.DataFrame(stats_data)
+    stats_data.to_csv(save_path)
+    print(stats_data)
--- plotting/process_results.py
+++ plotting/process_results.py
@@ -1,18 +1,23 @@
+"""
+This module draws the figures and performs statistical analysis of the results.
+"""
+
 import os
 
 from glob import glob
 import json
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 
-from grouped_boxplot import plot_grouped_boxplot
+from grouped_boxplot import plot_grouped_boxplot, compute_stats
 from constants import RED, BLUE, GREEN, BASELINE_LR, BASELINE_GP, GP_LR
 
 plt.style.use("ggplot")
 
 if not os.path.exists("figures"):
     os.mkdir("figures")
+
+P_ALPHA = 0.05
 
 df = []
 problems = []
@@ -39,6 +44,7 @@ with open("configurations.txt") as f:
                 print(f"sbatch learn_equations.sh configurations.txt {i}")
 
 # RQ1: Number of variables
+print("RQ1: Number of variables")
 lr_nrmse = list(df.groupby("num_vars")["lr_nrmse"].apply(list))
 gp_nrmse = list(df.groupby("num_vars")["gp_seed_nrmse"].apply(list))
 gp_lr_nrmse = list(df.groupby("num_vars")["gp_lr_nrmse"].apply(list))
@@ -50,14 +56,17 @@ plot_grouped_boxplot(
     labels=[BASELINE_LR, BASELINE_GP, GP_LR],
     colours=[RED, BLUE, GREEN],
     markers=["x", "o", "+"],
-    xticklabels=range(1, 10),
+    xticklabels=range(1, 11),
     xlabel="Number of variables",
     ylabel="NRMSE",
     # Need to hide fliers because LR errors are stupidly large
-    showfliers=False,
+    showfliers=[False, True, True],
 )
+compute_stats(df, "num_vars", P_ALPHA, "figures/rq1_num_vars_nrmse.csv")
+
 
 # RQ2: Amount of data
+print("\nRQ2: Amount of data")
 lr_nrmse = list(df.groupby("data_size")["lr_nrmse"].apply(list))
 gp_nrmse = list(df.groupby("data_size")["gp_seed_nrmse"].apply(list))
 gp_lr_nrmse = list(df.groupby("data_size")["gp_lr_nrmse"].apply(list))
@@ -73,10 +82,12 @@ plot_grouped_boxplot(
     xlabel="Number of data points",
     ylabel="NRMSE",
     # Need to hide fliers because LR errors are stupidly large
-    showfliers=False,
+    showfliers=[False, True, True],
 )
+compute_stats(df, "data_size", P_ALPHA, "figures/rq2_data_nrmse.csv")
 
 # RQ3: Amount of noise
+print("\nRQ3: Amount of noise")
 lr_nrmse = list(df.groupby("epsilon")["lr_nrmse"].apply(list))
 gp_nrmse = list(df.groupby("epsilon")["gp_seed_nrmse"].apply(list))
 gp_lr_nrmse = list(df.groupby("epsilon")["gp_lr_nrmse"].apply(list))
@@ -91,6 +102,28 @@ plot_grouped_boxplot(
     xticklabels=[0, 0.1, 0.25],
     xlabel="Epsilon",
     ylabel="NRMSE",
-    # Need to hide fliers because LR errors are stupidly large
-    showfliers=False,
+    # Need to hide fliers because a couple of LR errors are stupidly large
+    showfliers=[False, True, True],
 )
+compute_stats(df, "epsilon", P_ALPHA, "figures/rq3_epsilon_nrmse.csv")
+
+# Runtime
+print("\nRuntime")
+lr_time = list(df.groupby("num_vars")["lr_time"].apply(list))
+gp_time = list(df.groupby("num_vars")["gp_seed_time"].apply(list))
+gp_lr_time = list(df.groupby("num_vars")["gp_lr_time"].apply(list))
+
+assert len(lr_time) == 10
+
+plot_grouped_boxplot(
+    [lr_time, gp_time, gp_lr_time],
+    savepath="figures/runtime.png",
+    width=0.6,
+    labels=[BASELINE_LR, BASELINE_GP, GP_LR],
+    colours=[RED, BLUE, GREEN],
+    markers=["x", "o", "+"],
+    xticklabels=range(1, 11),
+    xlabel="Number of variables",
+    ylabel="Runtime (seconds)",
+)
+compute_stats(df, "num_vars", P_ALPHA, "figures/runtime.csv", outcome="time")
